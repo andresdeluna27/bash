0c46ed2b93149af232c6db60c0fb1ee4ebd9a59d
andresdeluna27 <paco_pokemaster12@hotmail.com>
2016-10-20 13:43:50 -0700

    ejemplos con if

---
+++ enXentos/si.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#####!/bin/bash es para identificar programacion bash
+if [ "abc" = "abc" ];then
+	echo son iguales
+fi
+#hay que tener cuidado con los espacios en el if, debe llevar espacios entre 
+#el signo =
---
+++ enXentos/si2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#####!/bin/bash es para identificar programacion bash
+if [ "abc" = "xbc" ];then
+	echo son iguales
+else
+	echo no son iguales
+fi
+#hay que tener cuidado con los espacios en el if, debe llevar espacios entre 
+#el signo =
