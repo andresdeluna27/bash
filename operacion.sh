9b834e4c8efc8f87b0c0391e26ff5d4713852e76
andresdeluna27 <paco_pokemaster12@hotmail.com>
2016-10-24 13:33:47 -0700

    operaciones en VI

---
+++ operacion.sh
@@ -0,0 +1,7 @@
+echo "ingresa numero"
+read num1
+echo "ingresa otro"
+read num2
+let num3=$num1/$num2
+echo el resultado es: $num3
+echo el resultado es $(($num1+$num2))
---
+++ operacion2.sh
@@ -0,0 +1,7 @@
+##se puede omitir el let en las asignaciones de a y b pero en las operaciones
+##no se puede omitir, como es el caso de c
+
+a=10
+b=20
+let c=$a+$b
+echo $c
